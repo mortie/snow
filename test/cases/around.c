f56cd48554e5107586006001912d80ebad7623d3
Martin Dørum <martid0311@gmail.com>
2018-08-24 03:08:25 +0200

    removed unnecessary workaround

--- test/cases/around.c
+++ test/cases/around.c
@@ -1,14 +1,3 @@
-// Create a fake gettimeofday which just increments with 1 second
-// every time it's called.
-/*
-static int currtime = 0;
-#define gettimeofday(t, n) \
-	do { \
-		(t)->tv_sec = currtime++; \
-		(t)->tv_usec = 0; \
-	} while (0)
-	*/
-
 #include <snow/snow.h>
 
 describe(a) {
